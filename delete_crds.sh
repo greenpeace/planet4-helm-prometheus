e820223bd6ea9a2def7ab04085a753d4b1945dd0
Roman O'Brien <robrien@greenpeace.org>
2026-03-02 13:35:56 +0100

    chore: update delete_crds.sh

--- delete_crds.sh
+++ delete_crds.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack#uninstall-helm-chart
 # CRDs created by this chart are not removed by default and should be manually
-# cleaned up.  # https://artifacthub.io/packages/helm/prometheus-worawutchan/kube-prometheus-stack/10.3.2
-# Kubernetes 1.15+
+kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd probes.monitoring.coreos.com
+kubectl delete crd prometheusagents.monitoring.coreos.com
 kubectl delete crd prometheuses.monitoring.coreos.com
 kubectl delete crd prometheusrules.monitoring.coreos.com
+kubectl delete crd scrapeconfigs.monitoring.coreos.com
 kubectl delete crd servicemonitors.monitoring.coreos.com
-kubectl delete crd podmonitors.monitoring.coreos.com
-kubectl delete crd alertmanagers.monitoring.coreos.com
 kubectl delete crd thanosrulers.monitoring.coreos.com
-kubectl delete crd probes.monitoring.coreos.com
